4cdaac95eef8c05c192fccafc311419cb19788c4
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2023-11-27 10:01:16 +0100

    ej7_ud4 corregido

--- ej7_ud4.sql
+++ ej7_ud4.sql
@@ -9,16 +9,17 @@
 --1.-(HR) Selecciona el número de empleados que fueron 
 --contratados en el año 1997 en la que trabajen en una oficina situada en Bélgica.
 SELECT COUNT(e.*)
-FROM employees e JOIN departments USING(department_id)
-	JOIN locations USING(location_id)
-	JOIN countries USING(country_id)
+FROM employees e RIGHT JOIN departments USING(department_id)
+	RIGHT JOIN locations USING(location_id)
+	RIGHT JOIN countries USING(country_id)
 WHERE country_name = 'Belgium'
 	AND DATE_PART('month' , hire_date) = 1997;
 	
 --2.-(HR) Selecciona la media de salario máximo 
 --de los trabajadores de Administration en Asia.
-SELECT AVG(salary)
-FROM employees e JOIN departments USING(department_id)
+SELECT AVG(max_salary)
+FROM employees e JOIN jobs USING(job_id)
+	JOIN departments USING(department_id)
 	JOIN locations USING(location_id)
 	JOIN countries USING(country_id)
 	JOIN regions USING(region_id)
@@ -38,6 +39,7 @@ WHERE t.nombre IN ('Casa' , 'Piso')
 	AND i.tipo_operacion = 'Alquiler'
 	AND provincia IN ('Huelva' , 'Sevilla' , 'Almería')
 	AND DATE_PART('month', fecha_operacion) IN (7,8,9)
+	--AND TO_CHAR(fecha_operacion, 'Q') = '3'
 ORDER BY fecha_operacion DESC;
 
 --4.-(INMO) Modifica la consulta anterior para que las viviendas que 
@@ -53,6 +55,7 @@ WHERE t.nombre IN ('Casa' , 'Piso')
 	AND provincia IN ('Huelva' , 'Sevilla' , 'Almería')
 	AND DATE_PART('month', fecha_operacion) IN (7,8,9)
 	AND AGE(fecha_alta , fecha_operacion) BETWEEN '35 day'::interval AND '45 day'::interval
+	--AND fecha_operacion BETWEEN fecha_alta + 35 AND fecha_alta + 45
 ORDER BY fecha_operacion DESC;
 
 --5.-(INMO) Calcula el precio máximo y precio mínimo por metro cuadrado 
@@ -64,14 +67,14 @@ FROM operacion JOIN inmueble i USING(id_inmueble)
 	JOIN tipo t ON(tipo_inmueble = id_tipo)
 WHERE t.nombre NOT IN ('Casa' , 'Piso')
 	AND tipo_operacion = 'Venta'
-	AND LOWER(provincia) ILIKE '%n%'
-	AND LENGTH(RTRIM(TO_CHAR(fecha_operacion , 'Month'),' ')) BETWEEN 5 AND 7;
+	AND provincia ILIKE '%n%'
+	AND LENGTH(RTRIM(TO_CHAR(fecha_operacion , '/*FM*/Month'),' ')) BETWEEN 5 AND 7;
 
 --6.- (VUELOS) ¿Cuál es el descuento medio obtenido en vuelos donde el origen sea 
 --España y el destino fuera de España, siempre que estos vuelos no se hayan 
 --realizado en fin de semana (contaremos como fin de semana los Viernes a partir de las 15:00)?
 
-SELECT ROUND(AVG(descuento), 2) || '€' AS "Precio medio de descuento"
+SELECT ROUND(AVG(COALESCE(descuento , 0)), 2) || '€' AS "Precio medio de descuento"
 FROM vuelo JOIN aeropuerto s ON(desde = s.id_aeropuerto)
 	JOIN aeropuerto l ON(hasta = l.id_aeropuerto)
 WHERE s.ciudad IN ('Sevilla' , 'Bilbao' , 'Barcelona' , 'Madrid', 'Málaga')
