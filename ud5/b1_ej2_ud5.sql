eafe0dd6c8869e327bf5a8a77c0ceb656641a8c6
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2023-12-10 20:25:57 +0100

    ej2_ud5_b1

--- ud5/b1_ej2_ud5.sql
+++ ud5/b1_ej2_ud5.sql
@@ -1,7 +1,3 @@
-/*
-Si suponemos que nuestro margen de beneficio con los productos es de un 25% (es decir, el 25% de su precio, son beneficios, y el 75% son costes), calcular la cantidad de beneficio que hemos obtenido, agrupado por categoría y producto. Las cantidades deben redondearse a dos decimales.
-Selecciona aquellos clientes (CUSTOMERS) para los que todos los envíos que ha recibido (sí, todos) los haya transportado (SHIPPERS) la empresa United Package.
-*/
 
 /*
 1.- Seleccionar el número de pedidos atendidos por
@@ -32,3 +28,28 @@ FROM categories JOIN products p USING(category_id)
 GROUP BY category_name
 ORDER BY COUNT(p.*) DESC
 LIMIT 1;
+
+
+/*
+4.- Si suponemos que nuestro margen de beneficio con 
+los productos es de un 25% (es decir, el 25% de su 
+precio, son beneficios, y el 75% son costes), calcular 
+la cantidad de beneficio que hemos obtenido, agrupado 
+por categoría y producto. Las cantidades deben 
+redondearse a dos decimales.
+*/
+SELECT category_name,product_name, ROUND(unit_price::numeric*units_in_stock::numeric*25/100,2)
+FROM products JOIN categories USING(category_id)
+GROUP BY category_name,product_name,ROUND(unit_price::numeric*units_in_stock::numeric*25/100,2)
+ORDER BY category_name;
+
+/*
+5.- Selecciona aquellos clientes (CUSTOMERS) para 
+los que todos los envíos que ha recibido (sí, todos) 
+los haya transportado (SHIPPERS) la empresa United Package.
+*/
+SELECT DISTINCT c.company_name
+FROM customers c JOIN orders USING(customer_id)
+	JOIN shippers s ON(shipper_id=ship_via)
+WHERE s.company_name = 'United Package'
+GROUP BY c.company_name;
