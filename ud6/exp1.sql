d43f0110337421915a93967dd491639651d6a4ec
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-01-08 10:03:50 +0100

    exp1

---
+++ ud6/exp1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE test;
+
+DROP DATABASE test;
+
+CREATE TABLE my_first_table (
+	first_column text,
+	second_column integer
+	);
+
+INSERT INTO my_first_table 
+VALUES ('Hola',1);
+
+CREATE TABLE productos (
+	num_producto integer,
+	nombre text,
+	precio numeric
+);
